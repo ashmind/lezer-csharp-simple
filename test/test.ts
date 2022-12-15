9237eb0f469a3f93adb49d3e9061cfbe89a37bc4
Andrey Shchekin <ashmind@gmail.com>
2022-12-15 21:55:18 +1300

    0.9.0 Finalize version and fix tests

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,10 @@
 import { fileTests } from '@lezer/generator/dist/test';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as url from 'url';
 import { parser } from '../dist/index.js';
 
-const caseDir = __dirname;
+const caseDir = url.fileURLToPath(new URL('.', import.meta.url).toString());
 
 for (let file of fs.readdirSync(caseDir)) {
   if (!/\.txt$/.test(file)) continue;
